949c6a86b26f6d0a132f751fb31e001364e08e0b
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-02-19 23:51:26 +0530

    coupon management-1

--- Low-level-design/SOLID/dependency-inversion/dependency-inversion.ts
+++ Low-level-design/SOLID/dependency-inversion/dependency-inversion.ts
@@ -3,12 +3,12 @@
 //  In the context of a Vehicle example, we can demonstrate this principle by using interfaces as abstractions for vehicle functionality.
 
 interface Engine {
-    start();
-    stop();
+    start(): void;
+    stop(): void;
 }
 
 interface Movement {
-    move();
+    move(): void;
 }
 
 class CarEngine implements Engine {
@@ -26,7 +26,7 @@ class MotorcycleEngine implements Engine {
         console.log("motorcycle enginer started");
     }
     stop() {
-        console.log("car enginer stopped.");
+        console.log("motorcycle engine stopped.");
 
     }
 }
@@ -54,4 +54,59 @@ class Car_di {
         this.engine = engine;
         this.movement = movement;
     }
+
+    start() {
+        this.engine.start();
+    }
+
+    stop() {
+        this.engine.stop();
+    }
+
+    move() {
+        this.movement.move();
+    }
 }
+
+class Motorcycle {
+    private engine: Engine;
+    private movement: Movement;
+
+    constructor(engine: Engine, movement: Movement) {
+        this.engine = engine;
+        this.movement = movement;
+    }
+
+    start() {
+        this.engine.start();
+    }
+
+    stop() {
+        this.engine.stop();
+    }
+    move() {
+        this.movement.move();
+    }
+}
+
+const carEngine = new CarEngine();
+const carMovement = new CarMovement();
+const car = new Car_di(carEngine, carMovement);
+
+const motorcycleEngine = new MotorcycleEngine();
+const motorcycleMovement = new MotocycleMovement();
+const motorcycle = new Motorcycle(motorcycleEngine, motorcycleMovement);
+
+car.start(); // Car engine started.
+car.move(); // Car moving.
+car.stop(); // Car engine stopped.
+
+motorcycle.start(); // Motorcycle engine started.
+motorcycle.move(); // Motorcycle moving.
+motorcycle.stop(); // Motorcycle engine stopped.
+
+// We define interfaces Engine and Movement to abstract the functionality of vehicle engines and movement.
+// Concrete classes CarEngine, MotorcycleEngine, CarMovement, and MotorcycleMovement implement these interfaces
+//  to provide specific implementations for cars and motorcycles. The Car and Motorcycle classes depend on the abstractions
+//  Engine and Movement, allowing us to easily change the engine or movement behavior without modifying the high-level classes.
+//  This demonstrates the Dependency Inversion Principle.
---
+++ Low-level-design/coupon-voucher-management/src/models/AccountStatus.ts
@@ -0,0 +1,4 @@
+export default class AccountStatus {
+    static ACTIVE = 1;
+    static INACTIVE = 1;
+}
